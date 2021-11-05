9cc81f4affc98888350b87e07c9654f87cf3d079
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-11-05 08:29:22 -0500

    start to theme change

--- Website/script.js
+++ Website/script.js
@@ -1,1 +1,15 @@
+//For Dark Mode
+let themeToggler = document.getElementById('theme-toggler');
+let themeIcon = document.getElementById('theme-icon');
+
+
+themeToggler.onclick = function(){
+	if (themeIcon.classList.contains('bi-sun')){
+		themeIcon.classList.remove('bi-sun');
+		themeIcon.classList.add('bi-moon');
+	}
+	 
+	
+}
+
 
