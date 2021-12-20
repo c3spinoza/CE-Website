f62bce5565dd8ba19c3b517a038f1c0d667a2777
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-12-20 14:50:00 -0600

    Hiding Unnecessary Togglers (pt. 1)
    I have figured that it would be best if I hide the togglers that i don't want  to show based on when the navigation bar expands. But, I am still figuring this out.

--- Website/script.js
+++ Website/script.js
@@ -1,15 +1,28 @@
-//For Dark Mode
-let themeToggler = document.getElementById('theme-toggler');
-let themeIcon = document.getElementById('theme-icon');
+//Theme Toggler
+let themeTogglerSm = document.getElementById('theme-toggler-sm');
+let themeTogglerMd = document.getElementById('theme-toggler-md');
+let screenWidth = window.screen.width
 
-
-themeToggler.onclick = function(){
-	if (themeIcon.classList.contains('bi-sun')){
-		themeIcon.classList.remove('bi-sun');
-		themeIcon.classList.add('bi-moon');
+//Hidding Uncessary Theme Togglers
+function hideTogglers() {
+	if (screenWidth < 767){
+    themeTogglerMd.style.display='none'
+	} else {
+    themeTogglerSm.style.display='none'
 	}
-	 
-	
 }
 
 
+//Swichting Icons When Theme is Changed
+themeTogglerSm.onclick = function(){
+	if (themeTogglerSm.classList.contains('bi-sun')){
+		themeTogglerSm.classList.remove('bi-sun');
+		themeTogglerSm.classList.add('bi-moon');
+	}
+}
+themeTogglerMd.onclick = function(){
+	if (themeTogglerMd.classList.contains('bi-sun')){
+		themeTogglerMd.classList.remove('bi-sun');
+		themeTogglerMd.classList.add('bi-moon');
+	}
+}
