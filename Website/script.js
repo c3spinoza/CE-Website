e17872e949df0cb068d297506e4625da551bb0ed
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-12-27 13:43:14 -0600

    back to the beginning
    I am back to where I started with the theme toggles hiding after the window loads and is resized.

--- Website/script.js
+++ Website/script.js
@@ -1,19 +1,21 @@
 //Theme Toggler
 let themeTogglerSm = document.getElementById('theme-toggler-sm');
 let themeTogglerMd = document.getElementById('theme-toggler-md');
-let screenWidth = window.screen.width;
+let screenWidth = window.screen.width
 
 //Hidding Uncessary Theme Togglers
-
-while (screenWidth < 768) {
-	themeTogglerMd.style.display='none';
-	themeTogglerSm.style.display='block';
+function hideToggler  {
+	if (screenWidth < 768) {
+		themeTogglerMd.style.display='none';
+		themeTogglerSm.style.display='block';
+	} else {
+		themeTogglerMd.style.display='block';
+		themeTogglerSm.style.display='none';
+	}
 }
+window.addEventListener('load', hideToggler);
+window.addEventListener('resize', hideToggler);
 
-while (screenWidth == 768 || screenWidth > 768) {
-	themeTogglerSm.style.display='none';
-	themeTogglerMd.style.display='block';
-}
 
 //Swichting Icons When Theme is Changed
 themeTogglerSm.onclick = function(){
@@ -21,10 +23,10 @@ themeTogglerSm.onclick = function(){
 		themeTogglerSm.classList.remove('bi-sun');
 		themeTogglerSm.classList.add('bi-moon');
 	}
-};
+}
 themeTogglerMd.onclick = function(){
 	if (themeTogglerMd.classList.contains('bi-sun')){
 		themeTogglerMd.classList.remove('bi-sun');
 		themeTogglerMd.classList.add('bi-moon');
 	}
-};
+}
