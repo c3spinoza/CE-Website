3225c3c2465a77d6252fd1c89141a5d7ceb1a62b
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-12-27 13:47:51 -0600

    Formatting

--- Website/script.js
+++ Website/script.js
@@ -5,28 +5,28 @@ let screenWidth = window.screen.width
 
 //Hidding Uncessary Theme Togglers
 function hideToggler() {
-	if (screenWidth < 768) {
-		themeTogglerMd.style.display='none';
-		themeTogglerSm.style.display='block';
-	} else {
-		themeTogglerMd.style.display='block';
-		themeTogglerSm.style.display='none';
-	}
+  if (screenWidth < 768) {
+    themeTogglerMd.style.display = 'none';
+    themeTogglerSm.style.display = 'block';
+  } else {
+    themeTogglerMd.style.display = 'block';
+    themeTogglerSm.style.display = 'none';
+  }
 }
 window.addEventListener('load', hideToggler);
 window.addEventListener('resize', hideToggler);
 
 
 //Swichting Icons When Theme is Changed
-themeTogglerSm.onclick = function(){
-	if (themeTogglerSm.classList.contains('bi-sun')){
-		themeTogglerSm.classList.remove('bi-sun');
-		themeTogglerSm.classList.add('bi-moon');
-	}
+themeTogglerSm.onclick = function() {
+  if (themeTogglerSm.classList.contains('bi-sun')) {
+    themeTogglerSm.classList.remove('bi-sun');
+    themeTogglerSm.classList.add('bi-moon');
+  }
 }
-themeTogglerMd.onclick = function(){
-	if (themeTogglerMd.classList.contains('bi-sun')){
-		themeTogglerMd.classList.remove('bi-sun');
-		themeTogglerMd.classList.add('bi-moon');
-	}
+themeTogglerMd.onclick = function() {
+  if (themeTogglerMd.classList.contains('bi-sun')) {
+    themeTogglerMd.classList.remove('bi-sun');
+    themeTogglerMd.classList.add('bi-moon');
+  }
 }
