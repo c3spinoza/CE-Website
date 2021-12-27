4ec2a30d46083dfef5b70ee58a9b9f74986652f3
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-12-27 14:53:10 -0600

    removed unecessary code
    I managed to get the them toggler to stay outside while everything else collapses. I won't need JS for this anymore.

--- Website/script.js
+++ Website/script.js
@@ -1,32 +1,10 @@
 //Theme Toggler
-let themeTogglerSm = document.getElementById('theme-toggler-sm');
-let themeTogglerMd = document.getElementById('theme-toggler-md');
-let screenWidth = window.screen.width
-
-//Hidding Uncessary Theme Togglers
-function hideToggler() {
-  if (screenWidth < 768) {
-    themeTogglerMd.style.display = 'none';
-    themeTogglerSm.style.display = 'block';
-  } else {
-    themeTogglerMd.style.display = 'block';
-    themeTogglerSm.style.display = 'none';
-  }
-}
-window.addEventListener('load', hideToggler);
-window.addEventListener('resize', hideToggler);
-
+let themeToggler = document.getElementById('theme-toggler');
 
 //Swichting Icons When Theme is Changed
-themeTogglerSm.onclick = function() {
+themeToggler.onclick = function() {
   if (themeTogglerSm.classList.contains('bi-sun')) {
     themeTogglerSm.classList.remove('bi-sun');
     themeTogglerSm.classList.add('bi-moon');
-  }
-}
-themeTogglerMd.onclick = function() {
-  if (themeTogglerMd.classList.contains('bi-sun')) {
-    themeTogglerMd.classList.remove('bi-sun');
-    themeTogglerMd.classList.add('bi-moon');
   }
 }
