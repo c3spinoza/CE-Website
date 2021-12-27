9ccbd80f3817a072a1d53da2e14bfb2332746955
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-12-26 19:08:28 -0600

    Added semicolons

--- Website/script.js
+++ Website/script.js
@@ -1,18 +1,18 @@
 //Theme Toggler
 let themeTogglerSm = document.getElementById('theme-toggler-sm');
 let themeTogglerMd = document.getElementById('theme-toggler-md');
-let screenWidth = window.screen.width
+let screenWidth = window.screen.width;
 
 //Hidding Uncessary Theme Togglers
 
 while (screenWidth < 768) {
-	themeTogglerMd.style.visibility='hidden'
-	themeTogglerSm.style.visibility='visible'
+	themeTogglerMd.style.visibility='hidden';
+	themeTogglerSm.style.visibility='visible';
 }
 
 while (screenWidth == 768 || screenWidth > 768) {
-	themeTogglerSm.style.visibility='hidden'
-	themeTogglerMd.style.visibility='visible'
+	themeTogglerSm.style.visibility='hidden';
+	themeTogglerMd.style.visibility='visible';
 }
 
 //Swichting Icons When Theme is Changed
@@ -21,10 +21,10 @@ themeTogglerSm.onclick = function(){
 		themeTogglerSm.classList.remove('bi-sun');
 		themeTogglerSm.classList.add('bi-moon');
 	}
-}
+};
 themeTogglerMd.onclick = function(){
 	if (themeTogglerMd.classList.contains('bi-sun')){
 		themeTogglerMd.classList.remove('bi-sun');
 		themeTogglerMd.classList.add('bi-moon');
 	}
-}
+};
