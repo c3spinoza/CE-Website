bf2e0804a1f98dcf71bcedf8c7341192cb4817c6
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2022-01-04 14:49:00 -0600

    Default Theme & Theme Toggler (Pt. 2)
    Made it so that the logo color changes when the theme changes. I also cleaned code a little for the change in color of the text and background, and of the the theme Toggler icon.

--- Website/script.js
+++ Website/script.js
@@ -4,7 +4,12 @@ let doc = document.getElementById('doc');
 let darkTheme = window.matchMedia('(prefers-color-scheme: dark)');
 //Theme Toggler
 let themeToggler = document.getElementById('theme-toggler');
+//Collapse Navabar Toggler 
 let collapseToggler = document.getElementById('collapse-toggler');
+//Navbar Logo
+ let navbarLogo = document.getElementById('navbar-logo');
+//Main Logo
+ let logo = document.getElementById('logo');
 
 
 //Setting Theme Acccording to System Theme
@@ -12,27 +17,27 @@ darkTheme.addEventListener('change', function(){
 	if (darktheme.matches) {
 		//Dark Theme 
 		
-		//Change Colors for Text & Cackgrounds
-		doc.classList.add('theme-dark');
+		//Change to Moon Icon 
+		themeToggler.classList.replace('bi-sun', 'bi-moon');
+		//Change Colors for Text & Backgrounds
+		doc.classList.replace('theme-light','theme-dark');
 		//Change color of Collapse Toggler
 		collapseToggler.style.backgroundImage = "url("+"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,255,255, 0.9)' stroke-width='3' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E"+");";
 		//Change Color of Logo 
-		
-		//Change to Moon Icon 
-		themeToggler.classList.remove('bi-sun');
-		themeToggler.classList.add('bi-moon');
+		navbarLogo.src = "CE_DesignV2.svg";
+		logo.src = "CE_DesignV2.svg";
 	} else {
 		//Light Theme
 		
-		//Change Colors for Text & Cackgrounds
-		doc.classList.add('theme-light');
+		//Change to Moon Icon 
+		themeToggler.classList.replace('bi-moon', 'bi-sun');
+		//Change Colors for Text & Backgrounds
+		doc.classList.replace('theme-dark','theme-light');
 		//Change color of Collapse Toggler
 		collapseToggler.style.backgroundImage = "url("+"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0,0,0, 0.9)' stroke-width='3' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E"+");";
 		//Change Color of Logo 
-		
-		//Change to Moon Icon 
-		themeToggler.classList.remove('bi-moon');
-		themeToggler.classList.add('bi-sun');
+		navbarLogo.src = "CE_DesignV1.svg";
+		logo.src = "CE_DesignV1.svg";
 	}
 });
 
@@ -43,26 +48,26 @@ themeToggler.onclick = function(){
 	if (themeToggler.classList.contains('bi-sun')){
 		//Dark Theme 
 		
-		//Change Colors for Text & Cackgrounds
-		doc.classList.add('theme-dark');
+		//Change to Moon Icon 
+		themeToggler.classList.replace('bi-sun', 'bi-moon');
+		//Change Colors for Text & Backgrounds
+		doc.classList.replace('theme-light','theme-dark');
 		//Change color of Collapse Toggler
 		collapseToggler.style.backgroundImage = "url("+"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,255,255, 0.9)' stroke-width='3' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E"+");";
 		//Change Color of Logo 
-		
-		//Change to Moon Icon 
-		themeToggler.classList.remove('bi-sun');
-		themeToggler.classList.add('bi-moon');
+		navbarLogo.src = "CE_DesignV2.svg";
+		logo.src = "CE_DesignV2.svg";
 	} else {
 		//Light Theme
 		
-		//Change Colors for Text & Cackgrounds
-		doc.classList.add('theme-light');
+		//Change to Moon Icon 
+		themeToggler.classList.replace('bi-moon', 'bi-sun');
+		//Change Colors for Text & Backgrounds
+		doc.classList.replace('theme-dark','theme-light');
 		//Change color of Collapse Toggler
 		collapseToggler.style.backgroundImage = "url("+"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0,0,0, 0.9)' stroke-width='3' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E"+");";
 		//Change Color of Logo 
-		
-		//Change to Moon Icon 
-		themeToggler.classList.remove('bi-moon');
-		themeToggler.classList.add('bi-sun');
+		navbarLogo.src = "CE_DesignV1.svg";
+		logo.src = "CE_DesignV1.svg";
 	}
 }
