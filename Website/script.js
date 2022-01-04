455cb2d114b9ca05674c496cad64f6ddf44b9f45
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2022-01-03 22:21:04 -0600

    Default Theme and Theme Toggler (Pt. 1)
    This is part one of the getting the default theme and theme toggler working. So far, the changing of the text and background color work as well as the initial part of the toggler icon changing.

--- Website/script.js
+++ Website/script.js
@@ -1,10 +1,65 @@
+//HTML Document
+let doc = document.getElementById('doc'); 
+//Sytem Theme
+let darkTheme = window.matchMedia('(prefers-color-scheme: dark)');
 //Theme Toggler
 let themeToggler = document.getElementById('theme-toggler');
 
-//Swichting Icons When Theme is Changed
-themeToggler.onclick = function() {
-  if (themeTogglerSm.classList.contains('bi-sun')) {
-    themeTogglerSm.classList.remove('bi-sun');
-    themeTogglerSm.classList.add('bi-moon');
-  }
+
+//Setting Theme Acccording to System Theme
+darkTheme.addEventListener('change', function(){
+	if (darktheme.matches) {
+		//Dark Theme 
+		
+		//Change Colors for Text & Cackgrounds
+		doc.classList.add('theme-dark');
+		//Change color of Collapse Toggler
+		
+		//Change Color of Logo 
+		
+		//Change to Moon Icon 
+		themeToggler.classList.remove('bi-sun');
+		themeToggler.classList.add('bi-moon');
+	} else {
+		//Light Theme
+    
+    //Change Colors for Text & Cackgrounds
+		doc.classList.add('theme-light');
+		//Change color of Collapse Toggler
+		
+		//Change Color of Logo 
+		
+		//Change to Moon Icon 
+		themeToggler.classList.remove('bi-moon');
+		themeToggler.classList.add('bi-sun');
+	}
+});
+
+//Swichting Theme with Toggler
+themeToggler.onclick = function(){
+	if (themeToggler.classList.contains('bi-sun')){
+		//Dark Theme 
+		
+		//Change Colors for Text & Cackgrounds
+		doc.classList.add('theme-dark');
+		//Change color of Collapse Toggler
+		
+		//Change Color of Logo 
+		
+		//Change to Moon Icon 
+		themeToggler.classList.remove('bi-sun');
+		themeToggler.classList.add('bi-moon');
+	} else {
+		//Light Theme
+    
+    //Change Colors for Text & Cackgrounds
+		doc.classList.add('theme-light');
+		//Change color of Collapse Toggler
+		
+		//Change Color of Logo 
+		
+		//Change to Moon Icon 
+		themeToggler.classList.remove('bi-moon');
+		themeToggler.classList.add('bi-sun');
+	}
 }
